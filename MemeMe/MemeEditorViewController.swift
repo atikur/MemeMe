e45d86d988c07bbde0da1fadc6f4bc084afeb0f8
Atikur Rahman <atik.bd08@gmail.com>
2016-05-02 08:26:13 +0600

    pick and display image

--- MemeMe/MemeEditorViewController.swift
+++ MemeMe/MemeEditorViewController.swift
@@ -8,6 +8,44 @@
 
 import UIKit
 
-class MemeEditorViewController: UIViewController {
+class MemeEditorViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    @IBOutlet weak var cameraButton: UIBarButtonItem!
+    @IBOutlet weak var memeImageView: UIImageView!
+    
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        cameraButton.enabled = UIImagePickerController.isSourceTypeAvailable(.Camera)
+    }
+    
+    @IBAction func cameraButtonPressed(sender: UIBarButtonItem) {
+        pickImageFromSource(.Camera)
+    }
+    
+    @IBAction func albumButtonPressed(sender: UIBarButtonItem) {
+        pickImageFromSource(.PhotoLibrary)
+    }
+    
+    // MARK: - UIImagePickerControllerDelegate Methods
+    
+    func imagePickerController(picker: UIImagePickerController, didFinishPickingImage image: UIImage, editingInfo: [String : AnyObject]?) {
+        memeImageView.image = image
+        dismissViewControllerAnimated(true, completion: nil)
+    }
+    
+    func imagePickerControllerDidCancel(picker: UIImagePickerController) {
+        dismissViewControllerAnimated(true, completion: nil)
+    }
+    
+    // MARK: - Helpers
+    
+    func pickImageFromSource(source: UIImagePickerControllerSourceType) {
+        let pickerController = UIImagePickerController()
+        pickerController.delegate = self
+        pickerController.sourceType = source
+        presentViewController(pickerController, animated: true, completion: nil)
+    }
+    
 }
 
